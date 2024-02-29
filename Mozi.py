334a55ca3a67f36208779975cd9a18cdeae61725
Levente4523 <hardilevi12@gmail.com>
2024-02-29 09:14:34 +0100

    A ketdetek

---
+++ Mozi.py
@@ -0,0 +1,13 @@
+from tkinter import * 
+from tkinter import messagebox
+from ttkbootstrap import *
+
+root = Tk()
+root.geometry("600x600")
+root.title("Mozitown")
+root.iconbitmap("./favicon.ico")
+
+root.configure(bg="#E6E6FF")
+
+
+root.mainloop()
