917ee68507299ac70d54b2204eac74739df0ab00
Levente4523 <hardilevi12@gmail.com>
2024-03-14 09:16:13 +0100

    vmi

--- Mozi.py
+++ Mozi.py
@@ -2,12 +2,32 @@ from tkinter import *
 from tkinter import messagebox
 from ttkbootstrap import *
 
-root = Tk()
-root.geometry("600x600")
+def fo():
+    tpl1 = Toplevel()
+    tpl1.geometry("1000x800")
+    tpl1.title("Mozitown")
+    tpl1.iconbitmap("./favicon.ico")
+    cim = Label(tpl1, text="Válassz filmet!", font=("Arial", 30))
+    cim.pack(pady=10)
+    tpl1.mainloop()
+
+
+#Főablak
+root = Window(themename='vapor')
+root.geometry("700x830")
 root.title("Mozitown")
 root.iconbitmap("./favicon.ico")
 
-root.configure(bg="#E6E6FF")
+kep = Image.open('J:\IKT (python)\Mozijegy\MOZITOWN.png')
+formazott = kep.resize((500, 750))
+megjelen = ImageTk.PhotoImage(formazott)
+logo = Label(root, image=megjelen)
+logo.pack()
 
+fogomb = Button(root, text="Válassz filmet", style="primary.outline.TButton", bootstyle="primary", command=fo)
+fogomb.pack()
 
-root.mainloop()
+mystyle = Style()
+mystyle.configure("primary.outline.TButton", font=("Arial", 24))
+
+root.mainloop() 
